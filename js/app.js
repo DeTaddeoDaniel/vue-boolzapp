d00dd8846f7176e0724a555cff4973bfdea62d52
Daniel2711 <detaddeo.d@gmail.com>
2020-12-17 17:20:39 +0100

    File base

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+var app = new Vue({
+
+    el: '#app',
+
+    data:{
+        message: 'Hello World',
+    },
+
+    methods: {
+        miaFunzione: function () {
+            //Codice
+        }
+    }
+
+}); 
